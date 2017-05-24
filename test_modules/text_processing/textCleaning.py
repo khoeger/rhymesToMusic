931c473415c9e8a773c20a74632d1af657832af1
khoeger <katarina.hoeger@gmail.com>
2017-05-23 21:19:16 -0400

    List Regexes to remove

---
+++ test_modules/text_processing/textCleaning.py
@@ -0,0 +1,11 @@
+"""
+    clean file of unwanted Regular Expressions
+"""
+
+# Unwanted RegExs
+unwantedRegexes =   ["%\\n",
+                    "\\xe2\\x80\\x94",
+                    "\\xe2\\x80\\x9c",
+                    "\\xe2\\x80\\x99",
+                    "\\xe2\\x80\\x9d"
+                    ]
