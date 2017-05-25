a6f7f53db5cf6fd5ae9ec81b8bf6494f687fceaa
khoeger <katarina.hoeger@gmail.com>
2017-05-24 22:19:51 -0400

    Attempt splitting lines

--- test_modules/text_processing/readTextFile.py
+++ test_modules/text_processing/readTextFile.py
@@ -2,15 +2,17 @@
     Create Function(s) That
     - Read in Files
 """
-# Used Libraries
-import re
-
 # Constants
 inputFile = "C:\\Users\\katar\\Documents\\2017\\music\\computerMusic\\projects\\rhymesToMusic\\test_modules\\sample_input\\raven.txt"
 encodingType = "IBM500"
+unwantedRegexes =   ["%\\n",
+                    "\\xe2\\x80\\x94",
+                    "\\xe2\\x80\\x9c",
+                    "\\xe2\\x80\\x99",
+                    "\\xe2\\x80\\x9d"
+                    ]
 
 # Helpers
-
 def engTextFromTxt(inputFile,encodingType):
     """
         read text file using western european fonts
@@ -20,8 +22,24 @@ def engTextFromTxt(inputFile,encodingType):
         rText = read_data.encode(encoding=encodingType, errors="strict")
         return(rText)
 
+def engTextFromTxt2(inputFile,encodingType,unwantedRegexes):
+    """
+        split file into new lines
+        read text file
+    """
+    with open(file=inputFile,mode='r', encoding=encodingType) as inputText:
+        read_data = inputText.read()
+        #rText = read_data.encode(encoding=encodingType, errors="strict")
+        rBreaks = read_data.split(unwantedRegexes[0])
+        print(len(rBreaks))
+        for i in rBreaks:
+            rText = type(i)#i.encode(encoding=encodingType, errors="strict")
+            print(rText)
+        #return(rText)
+
 #Testing
 testObj = engTextFromTxt(inputFile,encodingType)
+testObj2 = engTextFromTxt2(inputFile,encodingType,unwantedRegexes)
 
 print("\n \n \n")
 print(type(testObj))
--- test_modules/text_processing/textCleaning.py
+++ test_modules/text_processing/textCleaning.py
@@ -1,7 +1,7 @@
 """
     clean file of unwanted Regular Expressions
 """
-
+"""
 # Unwanted RegExs
 unwantedRegexes =   ["%\\n",
                     "\\xe2\\x80\\x94",
@@ -9,3 +9,18 @@ unwantedRegexes =   ["%\\n",
                     "\\xe2\\x80\\x99",
                     "\\xe2\\x80\\x9d"
                     ]
+
+# Attempt to place unwanted Regexes back into the stream
+
+
+# Import file
+from readTextFile import *
+# Used Libraries
+import re
+
+
+#--- Testing
+testObj = engTextFromTxt(inputFile,encodingType)
+w_breaks = testObj.split(unwantedRegexes[0])
+print("\n \n \n")
+print(w_breaks) """
