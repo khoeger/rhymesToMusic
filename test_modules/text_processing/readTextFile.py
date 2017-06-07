9e4408bd3ded860dd0985b0fdbad3aceb41386f2
khoeger <katarina.hoeger@gmail.com>
2017-06-07 17:09:21 -0400

    String output of input

--- test_modules/text_processing/readTextFile.py
+++ test_modules/text_processing/readTextFile.py
@@ -2,17 +2,20 @@
     Create Function(s) That
     - Read in Files
 """
+
 # Constants
 inputFile = "C:\\Users\\katar\\Documents\\2017\\music\\computerMusic\\projects\\rhymesToMusic\\test_modules\\sample_input\\raven.txt"
 inputFile2 = "C:\\Users\\katar\\Documents\\2017\\music\\computerMusic\\projects\\rhymesToMusic\\test_modules\\sample_input\\handkerchief.txt"
 inputFile3 = "C:\\Users\\katar\\Documents\\2017\\music\\computerMusic\\projects\\rhymesToMusic\\test_modules\\sample_input\\10things.txt"
-encodingType = "utf-8"#"IBM500"
+encodingType = "utf-8"#"IBM500" #"utf-8"
 unwantedRegexes =   ["%\\n",
                     "\\xe2\\x80\\x94",
                     "\\xe2\\x80\\x9c",
                     "\\xe2\\x80\\x99",
                     "\\xe2\\x80\\x9d"
                     ]
+
+#set PYTHONIOENCODING = utf-8
 
 # Helpers
 """def engTextFromTxt(inputFile,encodingType):
@@ -51,14 +54,15 @@ def engTextFromTxt3(inputFile,encodingType):
     #"
     with open(file=inputFile,mode='r+t', encoding=encodingType) as inputText:
         read_data = inputText.read()
-        print(len(read_data))
+        #print(type(read_data))
+        #print(len(read_data))
         rText = read_data.encode(encoding=encodingType, errors="strict")
-        print(type(read_data))
+        #print(type(rText))
     return(rText)
         #return(read_data)
 
 def engTextFromTxt4(inputFile,encodingType):
-    #"""
+    #""" DOESN"T WORK
     #    read text file using western european fonts
     #"""
     with open(file=inputFile,mode='r', encoding=encodingType) as inputText:
@@ -66,14 +70,29 @@ def engTextFromTxt4(inputFile,encodingType):
         inputText.close()
     return(read_data)
         #return(read_data)
-#"""
+
+def engTextFromTxt5(inputFile,encodingType):
+    #"
+        #read text file using western european fonts
+    #"
+    with open(file=inputFile,mode='r+t', encoding=encodingType) as inputText:
+        sText = inputText.read()
+        print(type(sText))
+        print(len(sText))
+        bText = sText.encode(encoding=encodingType, errors="backslashreplace") #errors="strict"
+        print(type(bText))
+        strText = bText.decode(encoding=encodingType, errors="backslashreplace")
+        print(type(strText))
+    #return(rText)
+    return(strText)
+"""
 #Testing
 #testObj = engTextFromTxt(inputFile,encodingType)
 #testObj2 = engTextFromTxt2(inputFile,encodingType,unwantedRegexes)
 #testObj3 = engTextFromTxt3(inputFile,encodingType)
-testObj4 = engTextFromTxt3(inputFile2,encodingType)
+testObj4 = engTextFromTxt3(inputFile3,encodingType)
 print("\n \n \n")
-print(type(testObj4))
+#print(type(testObj4))
 print("\n \n \n")
 print(testObj4)
-#"""
+"""
--- test_modules/text_processing/textCleaning.py
+++ test_modules/text_processing/textCleaning.py
@@ -7,11 +7,11 @@ unwantedRegexes =   ["%\\n",
                     "\\xe2\\x80\\x94",
                     "\\xe2\\x80\\x9c",
                     "\\xe2\\x80\\x99",
-                    "\\xe2\\x80\\x9d"
+                    "\\xe2\\x80\\x9d",
                     ]
 
 # Attempt to place unwanted Regexes back into the stream
-
+"""
 
 # Import file
 from readTextFile import *
@@ -20,7 +20,32 @@ import re
 
 
 #--- Testing
-testObj = engTextFromTxt(inputFile,encodingType)
-w_breaks = testObj.split(unwantedRegexes[0])
-print("\n \n \n")
-print(w_breaks) """
+testObj = engTextFromTxt3(inputFile3,encodingType)
+#w_breaks = testObj.split(unwantedRegexes[0])
+print("\n")# \n \n")
+#print(w_breaks)
+
+# Add appostrophes
+def byteToString( byteString ):
+    """ - Still has issues with other input
+        - runs using inputFile3 only currently
+        - works with 'python -E test_modules\\text_processing\\textCleaning.py PYTHONLEGACYWINDOWSSTDIO'
+    """
+    pattern = "\\xe2\\x80\\x99"
+    repl = "'"
+    bytesToString = byteString.decode(encodingType,errors="ignore")
+    #print(type(bytesToString))
+    #newString = re.sub(pattern, repl, bytesToString)
+    return(bytesToString)#newString)
+
+def removeEndPunct( inString ):
+    " take in string, output string without punctuation at ends of words
+    "
+
+#print(testObj)
+#print("\n \n \n")
+newString = byteToString(testObj)
+print(newString)
+print("\n")
+lNewString = newString.lower()
+print(lNewString)
