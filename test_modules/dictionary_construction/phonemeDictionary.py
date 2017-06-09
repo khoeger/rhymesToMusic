9676e1d502c1a5fee75de4069dc98f8e6e8fd166
khoeger <katarina.hoeger@gmail.com>
2017-06-08 20:55:43 -0400

    Initialize phonemeDictionary.py

---
+++ test_modules/dictionary_construction/phonemeDictionary.py
@@ -0,0 +1,14 @@
+"""
+Write the functions necessary for creating a phoneme dictionary
+- GENERALIZE IN FUTURE TO ALL DICTIONARIES
+"""
+# Imports
+from test_modules.text_processing.wordsToPhonemes import *
+# Constants
+
+# Variables
+
+# Helper functions
+
+# Testing
+print(cleanPList)
