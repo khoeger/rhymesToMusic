5046c3765c01b15e504a67571c171a9c9f8508f1
khoeger <katarina.hoeger@gmail.com>
2017-06-29 21:46:56 -0400

    Adding function to convert word chunk to measure

--- restructure/rhymesToMusic/parseTextAsMusic/parseText.py
+++ restructure/rhymesToMusic/parseTextAsMusic/parseText.py
@@ -9,10 +9,19 @@
         - read characters in string by "type"
         - types: words =[a-zA-Z]+ and [[a-zA-Z]+'[a-zA-Z]+], non words else
 """
+import nltk.corpus import cmudict
+import re
+import music21 as mus
+from rhymesToMusic.text_processing.wordsToPhonemes import *
+
+d = cmudict.dict()
+
+#-------------------------------
 
 #def phraseMaker(inString):
 #    """
 #    A phrase is a line w/out newline? Or with period.
+#    Break input section into phrases (Also need function to break into sections)
 #    """
 
 def buildMeasure(chunk):
@@ -24,3 +33,41 @@ def buildMeasure(chunk):
         - determine measure rhythm from chunk list length
         - build measure
     """
+    fPuncChunk = frontPunctuation(chunk)
+    tChunk = textChunk(chunk)
+    ePuncChunk = endPunctuation(chunk)
+
+    measure = mus.stream.Stream()
+    fPuncChunkOn = 0
+    texChunkOn = 0
+    ePuncChunkOn = 0
+    convertList = []
+
+    if fPuncChunk != '':
+        fPuncChunkOn = 1
+        convertList.append('REST')
+    if textChunk != '':
+        #textChunkOn = 1
+        textChunkPhonemes = word2Phoneme(chunk,d)
+        textChunkPhonemesClean = cleanPhonemeList(textChunkPhonemes)
+        #textChunkPhonemeLen = len(textChunkPhonemesClean)
+        convertList = convertList + textChunkPhonemesClean
+    if ePuncChunk != '':
+        ePuncChunkOn = 1
+        convertList.append('REST')
+
+    mParts = len(convertList)
+    noteLenth = 4/mParts
+
+    dur = mus.duration.Duration(noteLength)
+
+    for element in convertList:
+        if element == 'REST':
+            charRest = mus.note.Rest()
+            charRest.duration = dur
+            measure.append(charRest)
+        else:
+            pVal = phonemeToPitchVal(dictionary,phoneme)
+            pValNote = mus.note.Note(pVal)
+            pValNote.duration = dur
+            measure.append(pValNote)
