fefc177377251d8fcd079ae6aa9a83c3075c6b8c
khoeger <katarina.hoeger@gmail.com>
2017-09-05 15:51:24 -0400

    Add packages

--- restructure/rhymesToMusic/demo_5.Sept.2017.py
+++ restructure/rhymesToMusic/demo_5.Sept.2017.py
@@ -3,6 +3,15 @@ Run demo melody conversion
 """
 print("\n \n ")
 print("~Converting text to melody~\n")
+
+print("\n... Loading packages...\n")
+import re
+import music21 as mus
+from nltk.corpus import cmudict
+from nltk.tokenize import TweetTokenizer
+import pandas
+import pickle
+
 print(  "\nWhen inputting text, please do not use punctuation, apart",
         " from typically used contractions. \nPlease do not use slang.",
         " \nPlease use English.\n")
