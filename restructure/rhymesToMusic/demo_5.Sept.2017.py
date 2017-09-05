9478158115cfa9d62b9d7484784e4c8e5ecf25cc
khoeger <katarina.hoeger@gmail.com>
2017-09-05 15:47:58 -0400

    Add command line input functionality

--- restructure/rhymesToMusic/demo_5.Sept.2017.py
+++ restructure/rhymesToMusic/demo_5.Sept.2017.py
@@ -1,7 +1,14 @@
 """
 Run demo melody conversion
 """
-print("Converting text to melody")
-print("\nWhen inputting text, please do not use punctuation, apart",
-        " from typically used contractions. Please do not use slang.",
-        " Please use English.")
+print("\n \n ")
+print("~Converting text to melody~\n")
+print(  "\nWhen inputting text, please do not use punctuation, apart",
+        " from typically used contractions. \nPlease do not use slang.",
+        " \nPlease use English.\n")
+
+inputText = input("Please enter a string of text to convert: \n\n")
+
+scaleType = input("\nChoose a type of scale: \n    major\n    minor \n\n")
+
+print("\nGenerating a melody in a "+scaleType+" key using your input text.\n")
