aff15a114a442e57591b09f2e2bc3deafd454b46
khoeger <katarina.hoeger@gmail.com>
2017-09-05 15:30:51 -0400

    Add demo file

---
+++ restructure/rhymesToMusic/demo_5.Sept.2017.py
@@ -0,0 +1,7 @@
+"""
+Run demo melody conversion
+"""
+print("Converting text to melody")
+print("\nWhen inputting text, please do not use punctuation, apart",
+        " from typically used contractions. Please do not use slang.",
+        " Please use English.")
