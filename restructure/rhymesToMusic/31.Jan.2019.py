299f6feb1f72926c9e4f8963c5553fd87f07c8ac
khoeger <katarina.hoeger@gmail.com>
2019-01-31 13:49:50 -0500

    Update timestamp names

---
+++ restructure/rhymesToMusic/31.Jan.2019.py
@@ -0,0 +1,96 @@
+"""
+Run demo melody conversion
+- add token parsing of punctuation
+"""
+print("\n \n ")
+print("~Converting text to melody~\n")
+
+print("\n... Loading packages & modules ...\n")
+import re
+import music21 as mus
+from nltk.corpus import cmudict
+from nltk.tokenize import TweetTokenizer
+import pandas
+import pickle
+import sys
+import datetime
+
+from text_processing.readTextFile import *
+from text_processing.textCleaning import *
+from text_processing.wordsToPhonemes import *
+from dictionary_construction.phonemeDictionary import *
+from music_processing.read_corpus import *
+from music_processing.cleanPieces import *
+from dictionary_construction.noteFreqDictionary import *
+from numerical_operations.dictionaryEntries import *
+from numerical_operations.createAMPL import *
+from numerical_operations.outputToDict import *
+from parseTextAsMusic.parseText import *
+from dictionary_construction.dictToPandas import *
+
+print("\n... Setting constants ...\n")
+
+encodingType2 = "utf-8"#"IBM500" #"utf-8"
+corpus_chosen = 'airdsAirs'
+final_major_key = 'C5'#'F#4'
+final_minor_key = 'A4'#'Eb4'
+outpath = ( "C:\\Users\\katar\\Documents\\2019\\music\\computerMusic\\projects\\outputs\\rhymesToMusic\\")
+nameBeginning = "sample_"
+
+# --- User solicitedinput
+print(  "\nWhen inputting text, please do not use punctuation, apart",
+        " from typically used contractions. \nPlease do not use slang.",
+        " \nPlease use English.\n")
+
+inputText = "This is my sample string"#input("Please enter a string of text to convert: \n\n")
+
+scaleType = "major"#input("\nChoose a type of scale: \n    major\n    minor \n\n")
+
+outputName = nameBeginning+'{:%Y%m%d%H%M}'.format(datetime.datetime.now())#input("\nType an output file name. For example 'filename' will become filename.mid\n\n")
+print("\nGenerating a melody in a "+scaleType+" key using your input text.\n")
+
+# --- Pre calculated values
+d = cmudict.dict()
+tknzr = TweetTokenizer()
+if scaleType == 'major':
+    usedScale = mus.scale.MajorScale(final_major_key)
+elif scaleType == 'minor':
+    usedScale = mus.scale.MinorScale(final_minor_key)
+else:
+    print("The scale given, "+scaleType+", is not a recognized scale. Terminating.")
+    sys.exit()
+minorDict = {}
+majorDict = {}
+print(" ... ")
+
+# --- Start converting the inputText to music
+lowerInput = inputText.lower()
+wordList = tknzr.tokenize(lowerInput)
+phonemesOut = wordL2phonemeL(wordList,d)
+cleanList = cleanPhonemeList(phonemesOut[0])
+phonemeDictionary = tallyList(cleanList,{})
+print(" ... ")
+# Corups cleaning things skipped
+
+phonemeSeries = pandas.DataFrame(list(phonemeDictionary.items()),columns=["phoneme","# of occurences"])
+phonemeSeriesSum = phonemeSeries["# of occurences"].sum()
+phonemeSeries["percentages"] = phonemeSeries["# of occurences"]/phonemeSeriesSum
+phonemeSeries = phonemeSeries.sort_values(ascending=False,by="percentages")
+
+pDName = corpus_chosen+"_pdseries_"+scaleType+".pickle"
+structure = init_assignment_structure(pDName)
+sortedStructure = assignPhonemesToNotes(pDName,phonemeSeries)
+phonemeDict = createDictionary(sortedStructure)
+print(" ... ")
+
+readText = lowerInput
+chunkList = readText.split()
+
+musicOutpath = outpath + outputName +".mid"
+scoreOutpath = outpath + outputName +".ly"
+
+melodyOut = buildPiece(chunkList,d,phonemeDict,usedScale)
+melodyOut.write('midi',musicOutpath)
+melodyOut.write('lily',scoreOutpath)
+melodyOut.show('lily')
+melodyOut.show('midi')
